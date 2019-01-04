323e1f3e887f7677b5de11d0b776752f76e1a8e6
Vincelep <46317412+Vincelep@users.noreply.github.com>
2019-01-04 21:27:53 +0100

    exo 5

---
+++ exo_5.rb
@@ -0,0 +1,26 @@
+puts "On va compter le nombre d'heures de travail à THP"
+# ajout de # devant {...} pour que le calcul fontionne
+puts "travail : #{10 * 5 * 11}"
+# idem
+puts "En minutes ça fait : #{10*5*11*60}"
+#calcul suivant
+puts "et en seconde ?"
+#on eXecution du calcul. 
+puts 10 * 5 * 11 * 60 * 60
+#ajout de variable
+puts "est-ce que c'est vrai que 3 + 2 < 5 -7"
+#ruby cacul tout seul.
+puts 3 + 2 < 5 - 7
+#on décompose l'opération 
+puts "ça fait combien 3 + 2 ? #{3 + 2}"
+puts "ça fait combien 5 - 7 ? #{5 - 7}"
+#expérience d'une erreur
+puts "ok, c'est faux alors!"
+
+puts "c'est drôle ça, faisons-en plus :"
+#analyse de propositions 
+puts "est-ce que 5 est plus grand que - 2 ? #{5 > -2}"
+# >= sup ou égal
+puts "est-ce que 5 est suppérieur ou égal à -2 ? #{5 >= -2}"
+# <= inf ou égal
+puts "est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
